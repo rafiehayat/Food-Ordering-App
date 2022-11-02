d4f4e95c2a7bd7e0267c287cd198793f75adac05
Hayat <rafiehayat5@gmail.com>
2022-11-02 21:36:07 +0530

    AddingRealCartItems

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/Carticon";
 
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartctx = useContext(CartContext);
+
+  const numberOfCartItems = cartctx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
       <span></span>
     </button>
   );
